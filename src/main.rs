8bf53d65060171cd780eb97e4340d0027027589d
Maxence Frenette <maxence.frenette@gmail.com>
2018-07-19 08:41:49 -0400

    Add command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #[macro_use]
+extern crate clap;
+#[macro_use]
 extern crate lazy_static;
 extern crate tfe;
+
+use clap::{App, AppSettings, SubCommand};
 
 mod board;
 mod search;
@@ -9,23 +13,63 @@ use board::Board;
 use search::Search;
 
 fn main() {
-    let score = play_random_game();
-    println!("Final Score: {}", score);
+    let app = init_clap();
+    let matches = app.get_matches();
+
+    match matches.subcommand_name().unwrap() {
+        "play" => {
+            let score = play_random_game(true);
+            println!("Final Score: {}", score);
+        }
+        "bench" => {
+            let mut score = 0f64;
+
+            for i in 0..100 {
+                if i % 5 == 0 && i != 0 {
+                    println!("{}", i);
+                }
+
+                score += play_random_game(false) as f64;
+            }
+
+            println!("100 games played.");
+            println!("Average score: {}", score / 100.0);
+        }
+        _ => unreachable!(),
+    }
 }
 
-fn play_random_game() -> u64 {
+fn init_clap<'a, 'b>() -> App<'a, 'b> {
+    let play = SubCommand::with_name("play")
+        .about("plays one game, logging the board to the command line");
+    let bench =
+        SubCommand::with_name("bench").about("plays N games to test the strength of the AI");
+
+    App::new("My Super Program")
+        .author(crate_authors!(", "))
+        .version(crate_version!())
+        .about("A 2048 AI")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .subcommands(vec![play, bench])
+}
+
+fn play_random_game(verbose: bool) -> u64 {
     let mut board = Board::new();
     let mut search = Search::new();
 
-    println!("{:?}", board);
-    println!();
+    if verbose {
+        println!("{:?}", board);
+        println!();
+    }
 
     while !board.is_dead() {
         let mov = search.search(board.clone(), 2);
         board = board.make_move(&mov);
 
-        println!("{:?}", board);
-        println!();
+        if verbose {
+            println!("{:?}", board);
+            println!();
+        }
     }
 
     board.score()
