ec7d73c1a46ac725c9323837959d8a205b44b9f0
Maxence Frenette <maxence.frenette@gmail.com>
2018-07-20 11:32:11 -0400

    Fix sd forumla

--- src/stats.rs
+++ src/stats.rs
@@ -4,7 +4,10 @@ pub fn mean(sample: &Vec<f32>) -> f32 {
 
 pub fn standard_dev(sample: &Vec<f32>, x_0: f32) -> f32 {
     let n = sample.len() as f32;
-    (mean(&sample.iter().map(|x| square(x - x_0)).collect()) / (n - 1.)).sqrt()
+    let variance = mean(&sample.iter().map(|x| square(x - x_0)).collect()) / n;
+    let sd = (variance / n - 1.).sqrt();
+
+    sd
 }
 
 fn square(x: f32) -> f32 {
