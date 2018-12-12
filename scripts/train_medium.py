e024339e4da86b252f92d7e045bd9ec5dd2a1441
Maxence Frenette <maxence.frenette@gmail.com>
2018-12-11 21:30:32 -0500

    Adjust learning rate

--- scripts/train_medium.py
+++ scripts/train_medium.py
@@ -4,6 +4,6 @@ from shared import build, train, plot_many
 
 build()
 
-h = train("n_tuple_medium", num_games=1000000, alpha=0.0005, benchmark_interval=25000)
+h = train("n_tuple_medium", num_games=2000000, alpha=0.0025, benchmark_interval=25000)
 
 plot_many([("N-Tuple Medium", h)])
